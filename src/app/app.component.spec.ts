7d7fe92d3f394bbbf1ec63c9cd8fcb4d32dcb079
Martin van Dam <martinvandam@protonmail.com>
2023-08-04 11:38:55 +0200

    fix: update tests to make them pass

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -23,13 +19,15 @@ describe('AppComponent', () => {
   it(`should have as title 'ng-media-player'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-media-player');
+    expect(app.title).toEqual('Angular Media Player');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ng-media-player app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'Angular Media Player app is running!'
+    );
   });
 });
