c8d45029d84b0a7782c07daaaadb14b01543af1e
Martin van Dam <martinvandam@protonmail.com>
2023-11-15 09:31:58 +0100

    feat: use signals for search form

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, computed, signal } from '@angular/core';
 import { Video } from 'src/domain/video';
 import { VideoService } from '../video.service';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -10,8 +10,12 @@ import { EMPTY, Observable, filter } from 'rxjs';
   styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent implements OnInit {
-  searchQuery = '';
+  searchQuery = signal('');
   results$: Observable<Video[]> = EMPTY;
+  disabled = computed(() => {
+    console.log(this.searchQuery());
+    return this.searchQuery().length === 0;
+  });
 
   constructor(
     private videoService: VideoService,
@@ -20,16 +24,18 @@ export class SearchComponent implements OnInit {
   ) {}
 
   handleSubmit() {
-    this.router.navigate(['/search'], { queryParams: { q: this.searchQuery } });
+    this.router.navigate(['/search'], {
+      queryParams: { q: this.searchQuery() },
+    });
   }
 
   searchVideos() {
-    this.results$ = this.videoService.findByQuery(this.searchQuery);
+    this.results$ = this.videoService.findByQuery(this.searchQuery());
   }
 
   ngOnInit() {
     this.route.queryParams.pipe(filter((p) => p['q'])).subscribe((params) => {
-      this.searchQuery = params['q'];
+      this.searchQuery.set(params['q']);
       this.searchVideos();
     });
   }
